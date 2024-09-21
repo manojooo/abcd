6c32047d25d11255e8f051a28aa4a8a1cffa28fd
manojooo <vijjijovardhan@gmail.com>
2024-09-21 17:43:28 +0530

    manoj

---
+++ src/components/LoveLetter/LoveLetter.js
@@ -0,0 +1,49 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './abcd.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        mah dearuuu Manachaaaaa 🐶,<br />
+        Happiest Birthday to my speical one ❤️ <br />The Light Of my LIfe ✨<br />
+        I cherish every moment we spend together, and I want to spend my entire life with you.<br />
+        always yours<br />
+        @manojooo
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
